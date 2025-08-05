1e11aa230b2252b1ab65761184e9f132b590488d
Михаил Тихонов <mishkatix88@gmail.com>
2025-08-05 23:17:00 +0300

    Initial Commit

---
+++ BudgetTracker/BudgetTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTrackerApp.swift
+//  BudgetTracker
+//
+//  Created by Михайло Тихонов on 05.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct BudgetTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
